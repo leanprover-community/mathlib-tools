bdfd2088aba74331755c4fd7e6f56ec83168bdb7
Patrick Massot <patrickmassot@free.fr>
2019-05-13 18:41:18 +0200

    fix(debian_install): update repo location

--- scripts/install_debian.sh
+++ scripts/install_debian.sh
@@ -7,6 +7,6 @@ code --install-extension jroesch.lean
 wget https://raw.githubusercontent.com/Kha/elan/master/elan-init.sh
 bash elan-init.sh -y
 rm elan-init.sh
-wget https://raw.githubusercontent.com/leanprover-community/mathlib/master/scripts/remote-install-update-mathlib.sh
+wget https://raw.githubusercontent.com/leanprover-community/mathlib-tools/master/scripts/remote-install-update-mathlib.sh
 bash remote-install-update-mathlib.sh
 rm remote-install-update-mathlib.sh
