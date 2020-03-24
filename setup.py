44344e0544def4edde00b93a0da5e0f20000ac07
Patrick Massot <patrickmassot@free.fr>
2020-03-24 11:21:48 +0100

    Bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mathlibtools",
-    version="0.0.3",
+    version="0.0.4",
     author="The mathlib community",
     description="Lean prover mathlib supporting tools.",
     long_description=long_description,
